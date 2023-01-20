96579d3101740d71f18e5d1651a00c473c05fa42
Denzel Hopkins <hgmaasen@mnclz.org>
2023-01-20 13:35:17 +0100

    init commit

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,21 @@
+package demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class MessageResource {
+    @GetMapping("/")
+    fun index(): List<String> = listOf(
+       "This", "is", "my", "list"
+    )
+}
